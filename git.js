2f6f0b1e47c00cf1832237cf8027f5f3c0aa50d2
刘洋 <liuyang46@meituan.com>
2015-12-04 18:43:51 +0800

    git.js

---
+++ git.js
@@ -0,0 +1,20 @@
+/**
+ * Created by liuyang on 15/12/4.
+ */
+var process = require('child_process');
+var http = require("http");
+
+
+http.createServer(function (request, response) {
+
+    process.exec('git pull', function (err) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        if (err) {
+            response.write("pull error");
+        } else {
+            response.write('pull success');
+        }
+        response.end();
+    });
+
+}).listen(8888);
